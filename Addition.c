84fb3ca9f5080351c1e9403db7c5454d77635497
KalyaniPawar09 <112694661+KalyaniPawar09@users.noreply.github.com>
2022-09-20 21:33:15 +0530

    Addition Program

---
+++ Addition.c
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+
+   int a;
+   int b;
+   int c;
+   printf("Enter the value of a \n");
+   scanf("%d",a);
+   printf("Enter the value of b \n");
+   scanf("%d",b);
+   printf("Addition:",c=a+b);
+   printf("Addition=%d",c);
+   getch();
+
+}
