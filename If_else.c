bf9d3e0ed72e9bf219b18c6bd794b91014c2d718
KalyaniPawar09 <112694661+KalyaniPawar09@users.noreply.github.com>
2022-09-23 08:46:42 +0530

    IfElse Statement ic c

---
+++ If_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int age;
+printf("Enter The Age :");
+scanf("%d",&age);
+if(age>=18)
+{
+printf("The Person Is Eligible To Vote.");
+}
+else
+{
+printf("The Person Is Not ligible To Vote.");
+}
+getch();
+}
